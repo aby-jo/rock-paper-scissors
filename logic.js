b9079ae6d694ea743c82bd71957e4f871419f72b
aby-jo <173944472+aby-jo@users.noreply.github.com>
2024-07-22 01:59:07 +0530

    Added the playround function

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 console.log("Hello World")
 function getComputerChoice() {
     rand=100*Math.random();
-    output=undefined
+    let output
     if(rand<33){
         output="rock"
     }
@@ -15,6 +15,31 @@ function getComputerChoice() {
 }
 function getHumanChoice(){
     input=prompt("Enter your choice between Rock, Scissors and Paper")
-    console.log(input)
+    return input
 }
-getHumanChoice()
+let humanscore=0
+let computerscore=0
+function playRound(humanchoice,computerchoice){
+    humanchoice=humanchoice.toLowerCase()
+    let output
+    if(humanchoice==computerchoice){
+        output=`You both played ${humanchoice} so it is a draw `
+    }
+    else if(humanchoice=="rock" && computerchoice=="paper"){
+        output=`You lose as paper beats rock`
+        computerscore++
+    }
+    else if(humanchoice=="paper" && computerchoice=="scissors"){
+        output="You lose as paper as scissors beats paper" 
+        computerscore++
+    }
+    else if(humanchoice=="scissors" && computerchoice=="rock"){
+        output="You lose as rock beats scissors"
+        computerscore++
+    }
+    else{
+        output=`You win as ${humanchoice} beats ${computerchoice}`
+        humanscore++
+    }
+    return output
+}
