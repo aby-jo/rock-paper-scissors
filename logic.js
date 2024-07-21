51ebf39f17d24ca2858645cd087382f355e7ad89
aby-jo <173944472+aby-jo@users.noreply.github.com>
2024-07-22 02:21:30 +0530

    finsihed the full game

--- logic.js
+++ logic.js
@@ -1,45 +1,60 @@
 console.log("Hello World")
-function getComputerChoice() {
-    rand=100*Math.random();
-    let output
-    if(rand<33){
-        output="rock"
-    }
-    else if(rand<66){
-        output="scissors"
-    }
-    else{
-        output="paper"
-    }
-    return(output)
-}
-function getHumanChoice(){
-    input=prompt("Enter your choice between Rock, Scissors and Paper")
-    return input
-}
 let humanscore=0
 let computerscore=0
-function playRound(humanchoice,computerchoice){
-    humanchoice=humanchoice.toLowerCase()
-    let output
-    if(humanchoice==computerchoice){
-        output=`You both played ${humanchoice} so it is a draw `
+function playGame(){
+    function getComputerChoice() {
+        rand=100*Math.random();
+        let output
+        if(rand<33){
+            output="rock"
+        }
+        else if(rand<66){
+            output="scissors"
+        }
+        else{
+            output="paper"
+        }
+        return(output)
     }
-    else if(humanchoice=="rock" && computerchoice=="paper"){
-        output=`You lose as paper beats rock`
-        computerscore++
+    function getHumanChoice(){
+        input=prompt("Enter your choice between Rock, Scissors and Paper")
+        return input
     }
-    else if(humanchoice=="paper" && computerchoice=="scissors"){
-        output="You lose as paper as scissors beats paper" 
-        computerscore++
-    }
-    else if(humanchoice=="scissors" && computerchoice=="rock"){
-        output="You lose as rock beats scissors"
-        computerscore++
-    }
-    else{
-        output=`You win as ${humanchoice} beats ${computerchoice}`
-        humanscore++
-    }
-    return output
+    function playRound(humanchoice,computerchoice){
+        humanchoice=humanchoice.toLowerCase()
+        let output
+        if (humanchoice== "rock"||humanchoice=="scissors"||humanchoice=="paper"){
+        if(humanchoice==computerchoice){
+            output=`You both played ${humanchoice} so it is a draw `
+        }
+        else if(humanchoice=="rock" && computerchoice=="paper"){
+            output=`You lose as paper beats rock`
+            computerscore++
+        }
+        else if(humanchoice=="paper" && computerchoice=="scissors"){
+            output="You lose as paper as scissors beats paper" 
+            computerscore++
+        }
+        else if(humanchoice=="scissors" && computerchoice=="rock"){
+            output="You lose as rock beats scissors"
+            computerscore++
+        }
+        else{
+            output=`You win as ${humanchoice} beats ${computerchoice}`
+            humanscore++
+        }
+        console.log(output)
+        }
+        else(console.log("Wrong Input"))
+
 }
+
+    computerchoice=getComputerChoice()
+    humanchoice=getHumanChoice()
+    playRound(humanchoice,computerchoice)
+
+}
+for(let i=0;i<5;i++){
+    playGame()
+}
+console.log("Your score is",humanscore,"Computer Score is",computerscore)
